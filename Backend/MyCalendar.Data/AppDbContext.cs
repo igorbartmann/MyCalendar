d2da9994ee2ca37153b868850389dd64231e1b3f
erickbauer <contatoerickbauer@gmail.com>
2025-03-24 18:41:44 -0300

    Implemented DbContext

---
+++ Backend/MyCalendar.Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCalendar.Domain.Entities;
+
+namespace MyCalendar.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<CalendarEvent> CalendarEvents { get; set; }
+        public DbSet<Email> Emails { get; set; }
+        public DbSet<List> Lists { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<SharedArea> SharedAreas { get; set; }
+        public DbSet<SharedAreaUser> SharedAreaUsers { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
