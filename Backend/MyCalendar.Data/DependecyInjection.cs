b3004fc689ade8f4c20dadb83f6b47b7a0938210
erickbauer <contatoerickbauer@gmail.com>
2025-03-26 18:35:59 -0300

    Implemented the AddDbContextConfig method

---
+++ Backend/MyCalendar.Data/DependecyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyCalendar.Data
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddDbContextConfig(this IServiceCollection services, IConfiguration configurarion)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(configurarion.GetConnectionString("DefaultConnection"));
+            });
+
+            return services;
+        }
+    }
+}
