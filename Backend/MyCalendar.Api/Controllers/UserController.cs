0dda31d20ee96d4ebb90180493df58bbceffba82
erickbauer <contatoerickbauer@gmail.com>
2025-03-31 19:30:05 -0300

    Removing unnecessary using

--- Backend/MyCalendar.Api/Controllers/UserController.cs
+++ Backend/MyCalendar.Api/Controllers/UserController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using MyCalendar.Application.Interfaces.Services;
 using MyCalendar.Domain.DTOs.User;
 using MyCalendar.Domain.Entities;
--- Backend/MyCalendar.Domain/Entities/Response.cs
+++ Backend/MyCalendar.Domain/Entities/Response.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MyCalendar.Domain.Entities
+﻿namespace MyCalendar.Domain.Entities
 {
     public class Response<T>
     {
